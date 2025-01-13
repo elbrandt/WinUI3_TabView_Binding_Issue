f1af46b28f69b19fde8ba5f8ac056db66f8a6a83
Eric Brandt <eric.brandt@nikon.com>
2025-01-13 10:50:39 -0600

    initial commit

---
+++ ViewModel.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using CommunityToolkit.Mvvm.Input;
+
+namespace TabViewDemo
+{
+    public partial class MainViewModel
+    {
+        private static int _globalCounter = 0;
+        public ObservableCollection<TabViewModel> Tabs = [];
+
+        [RelayCommand]
+        private async Task TestAdd()
+        {
+            Tabs.Add(new TabViewModel(++_globalCounter));
+        }
+    }
+
+    public class TabViewModel
+    {
+
+        public TabViewModel(int number)
+        {
+            MyNumber = number;
+
+        }
+
+
+        public int MyNumber { get; }
+
+    }
+}
